e41fbab8c5b54fd2ffa547a374f062dcfad490d1
Josh Harmon <photigragraphy@gmail.com>
2020-07-30 12:35:49 -0700

    Paddle: Make the paddle movement a bit better. Still an issue where changing direction while holding the other direction causes the paddle to stop

--- BreakoutBasic/sprites/paddle.py
+++ BreakoutBasic/sprites/paddle.py
@@ -5,8 +5,9 @@ from sprites import DynamicSprite
 
 
 class Paddle(DynamicSprite):
-    _max_speed = 25
-    _friction = 0
+    _max_speed = 10
+    _friction = 0.5
+    _pressed = False
 
     def __init__(self):
         super().__init__(**{
@@ -16,21 +17,15 @@ class Paddle(DynamicSprite):
         })
 
     def handle_keyboard_event(self, event):
-        # print(f'Keyboard event: {event}')
+        # print(f'Keyboard event: {event.type}')
 
         if event.type == KEYS.KEYDOWN:
             if event.key in (KEYS.K_LEFT, KEYS.K_RIGHT):
-                self._friction = 0
-                if self.speed < self._max_speed:
-                    self.speed += 15
-                else:
-                    self.speed = self._max_speed
+                self._pressed = True
                 self.direction = (-1 if event.key == KEYS.K_LEFT else 1, 0)
 
         if event.type == KEYS.KEYUP:
-            self._friction = 1
-            if self.speed > 15:
-                self.speed -= 15
+            self._pressed = False
 
     def handle_mouse_event(self, event):
         # print(f'Mouse event: {event}')
@@ -42,6 +37,8 @@ class Paddle(DynamicSprite):
         x, y = self.position
         vel_x, _ = self.velocity
 
+        # print(f'Padde: {self.position} :: {self.direction} :: {self.velocity}')
+
         if x + size_x + vel_x > window_x:
             x = window_x - size_x
         elif x + vel_x < 0:
@@ -51,10 +48,12 @@ class Paddle(DynamicSprite):
         self.position = (x, y)
 
     def tick(self):
-        if self.speed > 0:
-            if self.speed < 1:
-                self.speed = 0
-            else:
+        if self._pressed:
+            self.speed = self._max_speed
+        else:
+            if self.speed > 0:
                 self.speed -= self._friction
+            else:
+                self.speed = 0
         if self.speed != 0:
             self.move()
